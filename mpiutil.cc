59bd47565366d553f08a44e1dc2aa8e3c4bff6f0
David Gleich <dfgleic@sandia.gov>
2010-11-10 17:06:59 -0800

    Fixed synchronized printf

--- mpiutil.cc
+++ mpiutil.cc
@@ -47,27 +47,30 @@ int mpi_world_printf(const char format[], ...)
 int mpi_sync_vprintf(MPI_Comm comm, const char format[], va_list args)
 {
     int rank, size;
-    int token=0;
     MPI_Comm_rank(comm,&rank);
     MPI_Comm_size(comm,&size);
+    const int maxlen=8191;
+    static char buffer[maxlen+1] = {0};
     
-    MPI_Barrier(comm);
-    
-    if (rank) {
+    if (!rank) {
         MPI_Status s;
-        MPI_Recv(&token,1,MPI_INT,rank-1,0,comm,&s);
+        
+        // the root processor starts by printing
+        vprintf(format, args);
+        
+        for (int i=1; i<size; i++) {
+            int bufsize;
+            memset(buffer, 0, sizeof(char)*(maxlen+1));
+            MPI_Recv(&bufsize, 1, MPI_INT, i, 0, comm, &s);
+            MPI_Recv(buffer, bufsize, MPI_CHAR, i, 0, comm, &s);
+            printf("%s", buffer);
+        }
+    } else {
+        int curlen = vsnprintf(buffer, maxlen, format, args);
+        MPI_Send(&curlen, 1, MPI_INT,0,0,comm);
+        MPI_Send(buffer, curlen, MPI_CHAR,0,0,comm);
     }
 
-    // always execute this code
-    vprintf(format, args);
-    fflush(stdout);
-    
-    if (rank+1!=size) {
-        MPI_Send(&token,1,MPI_INT,rank+1,0,comm);
-    }
-
-    MPI_Barrier(comm);
-    
     return (0);
 }
 
