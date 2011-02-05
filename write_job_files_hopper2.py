df22139fcb60bc82d06c632c8dc07c37f4492123
David Gleich <dgleich@cvrsvc05.(none)>
2011-02-05 14:27:55 -0800

    Fixed hopper2 job files

--- write_job_files_hopper2.py
+++ write_job_files_hopper2.py
@@ -4,8 +4,8 @@ import sys
 import os
 import math
 
-home_dir = '/panfs/scratch/home/dfgleic/'
-data_dir = os.path.join(home_dir,'data')
+home_dir = '/global/homes/d/dgleich/graph-db-eigs'
+data_dir = os.path.join(home_dir,'')
 prog_dir = os.path.join(home_dir,'graph_eigs')
 prog = os.path.join(prog_dir,'graph_eigs')
 
@@ -22,6 +22,7 @@ graphfile = os.path.join(data_dir,graph)
 nverts = graph_size(graphfile)
 
 nodemem = 32000000000
+nodecores = 24
 npernode = 24
 nthreads = 1
 
@@ -36,8 +37,8 @@ if len(sys.argv) > 2:
 else:    
     nmpiranks = 144 # must be a square, 6 nodes
 
-
 nodes = int(math.ceil(nmpiranks/float(npernode)))
+width = nodes*nodecores
     
 if len(sys.argv) > 3:
     time = sys.argv[3]    
@@ -47,6 +48,7 @@ else:
 print "nodes = %i"%(nodes)
 print "mpiranks = %i"%(nmpiranks)
 print "nprocs = %i"%(nodes*npernode*nthreads)
+print "width = %i"%(width)
 print "grid = %.1f x %.1f"%(math.sqrt(nmpiranks),math.sqrt(nmpiranks))
 perproc = int(math.ceil(float(nverts/math.sqrt(nmpiranks))))
 print "per-process = %i x %i"%(perproc,perproc)
@@ -57,13 +59,11 @@ assert(nodes > minnodes)
 
 # todo fix this
 params=dict(nodes=nodes, time=time,  npernode=npernode,
-    nthreads=nthreads,
+    nthreads=nthreads, width=width,
     nmpiranks=nmpiranks, prog=prog, graphfile=graphfile)
 script="""#!/bin/bash
-
-#PBS -l mppwidth=%(nmpiranks)i
-#PBS -l mppnppn=%(npernode)i
-#PBS -l mppdepth=%(nthreads)i
+#PBS -q regular
+#PBS -l mppwidth=%(width)i
 #PBS -l walltime=%(time)s
 #PBS -N %%s
 #PBS -e $PBS_JOBID.err
